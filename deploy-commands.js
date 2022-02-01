c2bdd1c017646736e06de948f604961057ecbf84
prosh14 <joshuasilva414@gmail.com>
2022-02-01 17:17:06 -0600

    demo commit

--- deploy-commands.js
+++ deploy-commands.js
@@ -11,6 +11,8 @@ const commands = [
 
 const rest = new REST({ version: '9' }).setToken(token);
 
+// Hi
+
 rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
 	.then(() => console.log('Successfully registered application commands.'))
 	.catch(console.error);
